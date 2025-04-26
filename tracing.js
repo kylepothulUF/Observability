52de2f74f1629c74d722248f1a6dccd60e2fc73b
kylepothulUF <126925197+kylepothulUF@users.noreply.github.com>
2025-04-25 20:48:27 -0400

    Add files via upload

--- tracing.js
+++ tracing.js
@@ -1,9 +1,9 @@
 const { resourceFromAttributes } = require('@opentelemetry/resources');
 const { ATTR_SERVICE_NAME } = require('@opentelemetry/semantic-conventions');
 const { NodeSDK } = require('@opentelemetry/sdk-node');
-const {
+/*const {
     getNodeAutoInstrumentations,
-} = require('@opentelemetry/auto-instrumentations-node');
+} = require('@opentelemetry/auto-instrumentations-node');*/
 const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-node');
 const {
     ConsoleMetricExporter,
@@ -17,19 +17,34 @@ const { MongoDBInstrumentation } =
     require("@opentelemetry/instrumentation-mongodb");
 const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
 
-const sdk = new NodeSDK({
-    resource: resourceFromAttributes({
-        [ATTR_SERVICE_NAME]: "todo-service"
-    }),
-    traceExporter: new ConsoleSpanExporter(),
-    metricReader: new PeriodicExportingMetricReader({
-        exporter: new ConsoleMetricExporter(),
-    }),
-    instrumentations: [
-        getNodeAutoInstrumentations(),
-        new ExpressInstrumentation(),
-        new MongoDBInstrumentation(),
-        new HttpInstrumentation()],
-});
+/*instrumentation.js*/
+const opentelemetry = require('@opentelemetry/sdk-node');
+const {
+  getNodeAutoInstrumentations,
+} = require('@opentelemetry/auto-instrumentations-node');
+const {
+  OTLPTraceExporter,
+} = require('@opentelemetry/exporter-trace-otlp-proto');
+const {
+  OTLPMetricExporter,
+} = require('@opentelemetry/exporter-metrics-otlp-proto');
+/*const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics');*/
 
+const sdk = new opentelemetry.NodeSDK({
+  traceExporter: new OTLPTraceExporter({
+    // optional - default url is http://localhost:4318/v1/traces
+    url: 'http://localhost:4318/v1/traces',
+    // optional - collection of custom headers to be sent with each request, empty by default
+    headers: {},
+  }),
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: new OTLPMetricExporter({
+      url: 'http://localhost:4318/v1/metrics', // url is optional and can be omitted - default is http://localhost:4318/v1/metrics
+      headers: {}, // an optional object containing custom headers to be sent with each request
+      concurrencyLimit: 1, // an optional limit on pending requests
+    }),
+  }),
+  instrumentations: [getNodeAutoInstrumentations()],
+});
 sdk.start();
+
